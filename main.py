554fe4dcf907f140832cdd1f339d540bef517fa2
Siavash <kosarisiavash4@gmail.com>
2022-12-20 21:06:08 +0330

    add layer class

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import random
+import numpy as np
+
+class Layer:
+    def __init__(self, activation : str, num_of_neurans : int):
+        self.activation : str = activation
+        self.num_of_neurans : int = num_of_neurans
+        self._initial_weights()
 
 class NeuralNetwork:
     def __init__(self, nu_inps, nu_nau_in_hid, num_outs):
@@ -6,14 +13,9 @@ class NeuralNetwork:
         self.nu_inps = nu_inps # number of inputs
         self.nu_nau_in_hid = nu_nau_in_hid # number of neurans in hidden layer
         self.num_outs = num_outs # number of outputs
-        self._initialize_hidden_layer() # initialize hidden layer (random weights)
-        self._initialize_output_layer() # initialize output layer (random weights)
 
-    def _initialize_hidden_layer(self):
-        self.layers.append([{'weights': [random.random() for i in range(self.nu_inps + 1)]} for j in range(self.nu_nau_in_hid)])
-
-    def _initialize_output_layer(self):
-        self.layers.append([{'weights': [random.random() for i in range(self.nu_nau_in_hid + 1)]} for j in range(self.num_outs)])
+    def add_layer(self, activation : str, num_of_neurans : int):
+        self.layers.append(Layer(activation = activation, num_of_neurans = num_of_neurans))
 
 
 
