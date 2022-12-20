bb9f38a457453092848da6eed8f017f1cec961a9
Siavash <kosarisiavash4@gmail.com>
2022-12-21 00:43:36 +0330

    created fit method in NeuralNetwork class

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from neuralnetwork import NeuralNetwork
 
 if __name__ == "__main__":
     nn = NeuralNetwork(nu_inps = 2, num_outs = 2)
+    nn.add_layer(activation = 'sigmoid', num_of_neurans = 4, num_of_inp_weights_to_each_neuran = 2)
+    nn.add_layer(activation = 'sigmoid', num_of_neurans = 2, num_of_inp_weights_to_each_neuran = 4)
--- neuralnetwork/network.py
+++ neuralnetwork/network.py
@@ -2,14 +2,14 @@ import random
 import numpy as np
 
 class Layer:
-    def __init__(self, activation : str, num_of_neurans : int, num_of_input_weights : int):
+    def __init__(self, activation : str, num_of_neurans : int, num_of_inp_weights_to_each_neuran : int):
         self.activation : str = activation
         self.num_of_neurans : int = num_of_neurans
-        self.num_of_input_weights : int = num_of_input_weights
+        self.num_of_inp_weights_to_each_neuran : int = num_of_inp_weights_to_each_neuran
         self._initial_weights()
 
     def _initial_weights(self):
-        self.weights = [[random.random() for i in range(self.num_of_neurans + 1)] for j in range(self.num_of_input_weights)]
+        self.weights = [[random.random() for i in range(self.num_of_neurans + 1)] for j in range(self.num_of_inp_weights_to_each_neuran)]
 
 class NeuralNetwork:
     def __init__(self, nu_inps, num_outs):
@@ -17,5 +17,12 @@ class NeuralNetwork:
         self.nu_inps = nu_inps # number of inputs
         self.num_outs = num_outs # number of outputs
 
-    def add_layer(self, activation : str, num_of_neurans : int):
-        self.layers.append(Layer(activation = activation, num_of_neurans = num_of_neurans))
+    def add_layer(self, activation : str, num_of_neurans : int, num_of_inp_weights_to_each_neuran : int):
+        self.layers.append(Layer(activation = activation, num_of_neurans = num_of_neurans, num_of_inp_weights_to_each_neuran = num_of_inp_weights_to_each_neuran))
+
+    def fit(self, x, y):
+        self.x = x
+        self.y = y
+
+    def train(self):
+        pass
