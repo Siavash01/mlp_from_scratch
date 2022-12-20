ea58cb191645fca91bd27cbdeb409e37c5e88647
Siavash <kosarisiavash4@gmail.com>
2022-12-21 02:42:14 +0330

    time to sleep

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ from neuralnetwork import NeuralNetwork
 
 if __name__ == "__main__":
     nn = NeuralNetwork(nu_inps = 2, num_outs = 2)
-    nn.add_layer(activation = 'sigmoid', num_of_neurans = 4, num_of_inp_weights_to_each_neuran = 2)
-    nn.add_layer(activation = 'sigmoid', num_of_neurans = 2, num_of_inp_weights_to_each_neuran = 4)
+    nn.fit(x = [1,2,3], y = [])
+    nn.add_layer(activation = 'sigmoid', num_of_neurans = 2, num_of_inp_weights_to_each_neuran = 3)
+    #nn.add_layer(activation = 'sigmoid', num_of_neurans = 2, num_of_inp_weights_to_each_neuran = 4)
+    print(nn.layers[0].neurans)
+    print("*****************")
+    nn.forward()
+    print(nn.layers[0].neurans)
--- neuralnetwork/network.py
+++ neuralnetwork/network.py
@@ -6,17 +6,18 @@ class Layer:
         self.activation : str = activation
         self.num_of_neurans : int = num_of_neurans
         self.num_of_inp_weights_to_each_neuran : int = num_of_inp_weights_to_each_neuran
+        self.neurans = [None for i in range(num_of_neurans)]
         self._initial_weights()
 
     def _initial_weights(self):
         self.weights = [[random.random() for i in range(self.num_of_neurans + 1)] for j in range(self.num_of_inp_weights_to_each_neuran)]
 
 class NeuralNetwork:
-    def __init__(self, nu_inps, num_outs, alpha = 0.1):
+    def __init__(self, nu_inps : int, num_outs : int, alpha : int = 0.1):
         self.layers = list()
         self.nu_inps = nu_inps # number of inputs
         self.num_outs = num_outs # number of outputs
-        self.alpha = alpha
+        self.alpha = alpha # learning rate
 
     def add_layer(self, activation : str, num_of_neurans : int, num_of_inp_weights_to_each_neuran : int):
         self.layers.append(Layer(activation = activation, num_of_neurans = num_of_neurans, num_of_inp_weights_to_each_neuran = num_of_inp_weights_to_each_neuran))
@@ -24,13 +25,19 @@ class NeuralNetwork:
     def fit(self, x, y, epochs = 1000):
         self.x = x
         self.y = y
+        self.epochs = epochs
 
     def forward(self):
-        for i in np.arange(0, len(self.layers)):
-            pass
+        for i in range(0, self.layers[0].num_of_neurans):
+            self.layers[0].neurans[i] = np.dot(self.x, self.layers[0].weights)
+        # for i in np.arange(0, self.nu_inps):
+        #     self.layers[0].neurans = np.dot(self.x, self.layers[0].weights)
+        # for i in np.arange(0, len(self.layers)):
+        #     pass
 
     def gradient_descent(self):
         pass
 
     def train(self):
-        pass
+        for i in range(self.epochs):
+            pass
