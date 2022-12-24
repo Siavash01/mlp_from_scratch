b19433dbd032ffc6748423dca636c813ace3db85
Siavash <kosarisiavash4@gmail.com>
2022-12-24 14:14:51 +0330

    forward done

--- neuralnetwork/network.py
+++ neuralnetwork/network.py
@@ -2,16 +2,16 @@ import random
 import numpy as np
 
 class Layer:
-    def __init__(self, activation : str, num_of_neurans : int, num_of_inp_weights_to_each_neuran : int):
+    def __init__(self, activation : str, num_of_neurons : int, num_of_inp_weights_to_each_neuran : int):
         self.activation : str = activation
-        self.num_of_neurans : int = num_of_neurans
+        self.num_of_neurons : int = num_of_neurons
         self.num_of_inp_weights_to_each_neuran : int = num_of_inp_weights_to_each_neuran
-        self.neurans = [None for i in range(num_of_neurans)]
+        self.neurons = [None for i in range(num_of_neurons)]
         self._initial_weights()
 
     def _initial_weights(self):
-        # self.weights = [[random.random() for i in range(self.num_of_neurans + 1)] for j in range(self.num_of_inp_weights_to_each_neuran)]
-        self.weights = [[random.random() for i in range(self.num_of_neurans)] for j in range(self.num_of_inp_weights_to_each_neuran)] # no bias
+        # self.weights = [[random.random() for i in range(self.num_of_neurons + 1)] for j in range(self.num_of_inp_weights_to_each_neuran)]
+        self.weights = [[random.random() for i in range(self.num_of_neurons)] for j in range(self.num_of_inp_weights_to_each_neuran)] # no bias
 
 class NeuralNetwork:
     def __init__(self, nu_inps : int, num_outs : int, alpha : int = 0.1):
@@ -20,8 +20,8 @@ class NeuralNetwork:
         self.num_outs = num_outs # number of outputs
         self.alpha = alpha # learning rate
 
-    def add_layer(self, activation : str, num_of_neurans : int, num_of_inp_weights_to_each_neuran : int):
-        self.layers.append(Layer(activation = activation, num_of_neurans = num_of_neurans, num_of_inp_weights_to_each_neuran = num_of_inp_weights_to_each_neuran))
+    def add_layer(self, activation : str, num_of_neurons : int, num_of_inp_weights_to_each_neuran : int):
+        self.layers.append(Layer(activation = activation, num_of_neurons = num_of_neurons, num_of_inp_weights_to_each_neuran = num_of_inp_weights_to_each_neuran))
 
     def fit(self, x, y, epochs = 1000):
         self.x = x
@@ -30,13 +30,21 @@ class NeuralNetwork:
 
     def forward(self):
         '''
-        matrix multipication of inputs and first hidden layer weigts witch does the job of creating first hidden layer nerurans
+        matrix multipication of inputs and first hidden layer weigts witch does the job of creating first hidden layer neurons
         '''
-        self.layers[0].neurans = np.dot(self.x, self.layers[0].weights)
-        # for i in range(0, self.layers[0].num_of_neurans):
-        #     self.layers[0].neurans = np.dot(self.x, self.layers[0].weights)
+        # self.layers[0].neurons = np.dot(self.x, self.layers[0].weights)
+        # self.layers[1].neurons = np.dot(self.layers[0].neurons, self.layers[1].weights)
+
+        # alternative
+
+        self.layers[0].neurons = np.dot(self.x, self.layers[0].weights)
+        for i in range(1, len(self.layers)):
+            self.layers[i].neurons = np.dot(self.layers[i-1].neurons, self.layers[i].weights)
+
+        # for i in range(0, self.layers[0].num_of_neurons):
+        #     self.layers[0].neurons = np.dot(self.x, self.layers[0].weights)
         # for i in np.arange(0, self.nu_inps):
-        #     self.layers[0].neurans = np.dot(self.x, self.layers[0].weights)
+        #     self.layers[0].neurons = np.dot(self.x, self.layers[0].weights)
         # for i in np.arange(0, len(self.layers)):
         #     pass
 
